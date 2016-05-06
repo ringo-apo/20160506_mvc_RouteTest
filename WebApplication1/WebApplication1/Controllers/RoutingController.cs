907769383b5da718898054264208f2e1fc14bf13
APO <ringo1625@gmail.com>
2016-05-06 20:07:29 +0900

    Routingを追加

---
+++ WebApplication1/WebApplication1/Controllers/RoutingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class RoutingController : Controller
+    {
+        // GET: Routing
+        public ActionResult Index(int? id,int? id2)
+        {
+            ViewBag.id = id;
+            ViewBag.id2 = id2;
+            return View();
+        }
+    }
+}
